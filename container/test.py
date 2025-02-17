e9328d338663a8397cfc95d379e26781c1d71e2b
Vibhor Jain <jain.vibhorjv@hotmail.com>
2025-02-17 13:35:23 -0500

    updates for defining new stack for s3, lambda, congnito pool and new git action for deploy new stack and clean up stack

--- container/test.py
+++ container/test.py
@@ -1,12 +1,16 @@
 import requests
 import json
-
+import os 
 # Define the API endpoint URL
 api_url = 'http://127.0.0.1:5000/predict'
 
 # Read input data from JSON file
-with open('./container/input_data.json', 'r') as input_file:
+with open('./container/input_test_data/policy_summary_data.json', 'r') as input_file:
     data = json.load(input_file)
+
+output_dir = './container/output_test_data'
+os.makedirs(output_dir, exist_ok=True)
+preidcted_policy_summary_data_path = os.path.join(output_dir, 'pridcted_policy_summary_data.json')
 
 try:
     # Send POST request to the API
@@ -18,7 +22,7 @@ try:
         result = response.json()
 
         # Write the output to a JSON file
-        with open('output_data.json', 'w') as output_file:
+        with open(preidcted_policy_summary_data_path, 'w') as output_file:
             json.dump(result, output_file, indent=2)
         
         print("Predictions have been saved to 'output_data.json'.")
